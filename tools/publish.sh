bb6b6369bbbc9c061e9ad7f69cea99aa8cafee32
Kamisatoooooo <kamisatoooooo@gmail.com>
2026-06-08 17:36:30 +0800

    update publish script

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+if [[ $# -eq 0 ]]
+then
+    exit 0
+fi
+
 if [[ $1 =~ ^_drafts.* ]]
-then bundle exec jekyll publish $1
+then bundle exec jekyll publish "$1"
 else
     bundle exec jekyll publish "_drafts/$1"
 fi
